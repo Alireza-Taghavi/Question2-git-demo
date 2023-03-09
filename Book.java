dcfda8e81001a61ca5e07b622bd4b448a7f54a7e
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2023-03-09 14:42:14 +0330

    first commit

--- Book.java
+++ Book.java
@@ -1,2 +1,23 @@
-public class Book {
+public abstract class Book {
+    private String title;
+    private double price;
+
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract void setPrice(double price);
 }
